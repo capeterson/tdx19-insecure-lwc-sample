1d8128c8275cbfe91f88b247250e060984a4736e
Chris Peterson <chris.peterson@salesforce.com>
2019-05-21 12:07:35 -0700

    Initial commit of component MVP

---
+++ README.md
@@ -0,0 +1,2 @@
+# SFDX App
+A silly little LWC used in a TDX19 talk. This is insecure by desgin and should never be used in an org you care about.
---
+++ force-app/main/default/lwc/quickcreate/quickcreate.js
@@ -0,0 +1,26 @@
+import { LightningElement, api } from 'lwc';
+import create from '@salesforce/apex/QuickCreateController.create';
+import Opportunity from '@salesforce/schema/Opportunity';
+
+export default class Quickcreate extends LightningElement {
+    opp;
+    error;
+
+    constructor(){
+        super();
+        this.opp = {
+            'sobjectType' : 'Opportunity',
+            'StageName': 'Prospecting'
+        };
+    }
+
+    handleFieldChange(event){
+        let field = event.target.name;
+        this.opp[field] = event.target.value;
+    }
+    saveRecord(){
+        create({opp: this.opp})
+            .then( result => { this.opp.result; })
+            .catch(error => {this.error = error;});
+    }
+}
